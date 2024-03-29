09a7a37dd6ad9bc8dd3fd73bea6171b19e5ba7c8
tims-exe <timonkjohn@gmail.com>
2024-03-29 10:35:20 +0530

    latest update

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+//express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// listen for request
+app.listen(process.env.PORT, () =>{
+    console.log(`Listening on port : ${process.env.PORT}`);
+})
